0b0566a6d0c2b611db196d9d4f61f62b39bacfb8
SalazarFranco <francosalazar057@gmail.com>
2024-12-18 10:44:01 -0500

    creacion de proyecto del examen

---
+++ src/main/java/edu/com/francoSalazar/CL2_DAWI_Zalazar_Lopez/Cl2DawiZalazarLopezApplication.java
@@ -0,0 +1,13 @@
+package edu.com.francoSalazar.CL2_DAWI_Zalazar_Lopez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cl2DawiZalazarLopezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cl2DawiZalazarLopezApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/com/francoSalazar/CL2_DAWI_Zalazar_Lopez/Cl2DawiZalazarLopezApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.com.francoSalazar.CL2_DAWI_Zalazar_Lopez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cl2DawiZalazarLopezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
